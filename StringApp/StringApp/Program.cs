5cb7b5e545bca79e7e2428f2265b854707ddedcd
liming zhao <Lina@Torchlights-MacBook-Pro.local>
2016-09-18 14:00:52 -0400

    source commit start

---
+++ StringApp/StringApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StringApp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			//reverseWords("Hello worlds!");
+			String srcTest = "Hello my friends";
+			String toRemove = "my";
+
+			String result = RemoveChar.removeChar (srcTest, toRemove);
+			Console.WriteLine (result);
+		}
+
+
+		private static void reverseWords(String str)
+		{
+			String[] words = str.Split (null);
+			String[] reversedWords = reverseWordsArray (words);
+
+			printStr (reversedWords);
+		}
+
+		public static string[] reverseWordsArray(String[] words)
+		{
+			int i, j = words.Length;
+			for (i = 0, j = words.Length  - 1; i < j; i++, j--) {
+
+				String temp = words [i];
+				words [i] = words [j];
+				words [j] = temp;
+
+			}
+			return words;
+		}
+
+		private static void printStr(String[] words)
+		{
+			for (int i = 0; i < words.Length; i++) {
+				Console.WriteLine (words [i]);
+			}
+		}
+
+	}
+}
---
+++ StringApp/StringApp/RemoveChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StringApp
+{
+	public class RemoveChar
+	{
+
+	
+		public static String removeChar (String str, String remove)
+		{
+			char[] s = str.ToCharArray ();
+			char[] r = remove.ToCharArray ();
+			bool[] flag = new bool[128];
+			int len = r.Length;
+			int src = 0 , dst = 0;
+
+			for(int i =0; i < len; i++)
+			{
+				flag[r[i]] = true;
+			}
+
+			while (src < s.Length) {
+				
+				if(!flag[s [src]])
+				{
+					s [dst++] = s [src];
+				}
+				++src;
+
+			}
+
+			return new string (s, 0, dst);
+
+
+
+
+		}
+	}
+}
+
